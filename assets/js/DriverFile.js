ec7fcfec6c803aaca4e9c1dc26c1761b090c554b
Michael Pedzimaz <mpedzi03@gmail.com>
2021-05-12 17:03:17 -0500

    _JSPractice: aesthetic problem work

--- assets/js/DriverFile.js
+++ assets/js/DriverFile.js
@@ -4,9 +4,27 @@ let obj = new AestheticTreeProblem([1,3,4,1])
 
 let result = obj.performTreeCheck()
 
-console.log(result)
-
+document.getElementById('aesthetic-tree-input').addEventListener('keypress', aestheticTreeInput)
+document.getElementById('aesthetic-tree-button').addEventListener('click', aestheticTreeStart)
 
 function aestheticTreeInput(e) {
-    console.log(e)
+  const numCommaRegex = /^\d|\,$/g
+  if (!numCommaRegex.test(e.key)) e.preventDefault()
+}
+
+function aestheticTreeStart(e) {
+  let inputEl = document.getElementById('aesthetic-tree-input')
+  let aestheticTreeTestData = inputEl.value
+  let numArr = aestheticTreeTestData.split(',')
+
+  let numArr2 = numArr.map((val) => {
+    return parseInt(val)
+  })
+
+  let obj = new AestheticTreeProblem(numArr2)
+
+  let result = obj.performTreeCheck()
+
+  let resultEl = document.getElementById('aesthetic-tree-result')
+  resultEl.value = result
 }
